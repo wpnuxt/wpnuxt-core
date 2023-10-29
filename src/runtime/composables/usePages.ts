13e68aa5aaff337f56589547af383a161327e49e
Wouter Vernaillen <wouter@vernaillen.com>
2023-10-29 09:13:13 +0100

    added Pages query

---
+++ src/runtime/composables/usePages.ts
@@ -0,0 +1,27 @@
+import { useNuxtData, ref, useFetch, createError } from "#imports"
+
+const _usePages = async () => {
+    const cacheKey = 'allPages'
+    const cachedPages = useNuxtData(cacheKey)
+    const pages = ref()
+
+    if (cachedPages.data.value) {
+        pages.value = cachedPages.data.value
+    } else {
+        const { data, refresh, pending, error } = await useFetch("/api/graphql_middleware/query/Pages", {
+            key: cacheKey,
+            transform (data: any) {
+                return data.data.pages.nodes;
+            }
+        });
+        if (error.value) {
+            throw createError({ statusCode: 500, message: 'Error fetching pages', fatal: true })
+        }
+        pages.value = data.value
+    }
+    return {
+        data: pages.value
+    }
+}
+
+export const usePages = _usePages
