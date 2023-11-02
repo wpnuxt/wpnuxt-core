7b9d9c2c9693e31d49001df48c8a0af15fcdaea5
Wouter Vernaillen <wouter@vernaillen.com>
2023-11-02 11:02:33 +0100

    nuxt image

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ['../src/module', "@nuxt/image"],
   wpNuxt: {
     wordpressUrl: 'https://wpnuxt.vernaillen.com',
     showBlockInfo: false,
