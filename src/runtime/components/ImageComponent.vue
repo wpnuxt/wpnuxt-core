1ec1711ce002a526b3271ad2eb356b9c73177e8f
Wouter Vernaillen <wouter@vernaillen.com>
2023-11-02 11:34:46 +0100

    import useRuntimeConfig

--- src/runtime/components/ImageComponent.vue
+++ src/runtime/components/ImageComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from '#imports';
+import { ref, useRuntimeConfig } from '#imports';
 
 const config = useRuntimeConfig();
 const wpUrl = config.public.wpNuxt.wordpressUrl
