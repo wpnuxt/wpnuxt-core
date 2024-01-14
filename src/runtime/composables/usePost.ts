99ce23230b646005429898752c35beb48ea995e2
Wouter Vernaillen <wouter@vernaillen.com>
2024-01-14 14:55:16 +0700

    WPNUX-7 enable cache in composable

--- src/runtime/composables/usePost.ts
+++ src/runtime/composables/usePost.ts
@@ -1,30 +1,55 @@
-import { ref, computed, useNuxtData, useFetch, createError, useWPNuxtLogger } from "#imports"
+import { ref, computed, useNuxtData, useTokens, useFetch, createError, useWPNuxtLogger } from "#imports"
 
 const _usePostByUri = async (uri: string) => {
     const logger = useWPNuxtLogger()
-    const { data, pending, refresh, error } = await useFetch("/api/graphql_middleware/query/PostByUri/", {
-        params: {
-            uri: uri
-        },
-        transform (data) {
-            return data.data.nodeByUri;
-        }
-    })
-    if (error.value) {
-        logger.error('usePostByUri, error: ', error.value)
-        throw createError({ statusCode: 500, message: 'Error fetching PostByUri', fatal: true })
+    const post = ref()
+    const cacheKey = computed(() => `postByUri-${uri}`)
+    const tokens = useTokens()
+    const cachedPost = useNuxtData(cacheKey.value)
+
+    logger.debug('usePostByUri, fetching post for uri: ', uri)
+
+    if (cachedPost.data.value) {
+      post.value = cachedPost.data.value
+      logger.debug('usePostByUri, got post from cache: ', post.value.title)
+    } else {
+      const { data, pending, refresh, error } = await useFetch("/api/graphql_middleware/query/PostByUri/", {
+          key: cacheKey.value,
+          params: {
+              uri: uri
+          },
+          headers: {
+            Authorization: tokens.authorizationHeader
+          },
+          transform (data) {
+              return data.data.nodeByUri;
+          }
+      })
+      if (error.value) {
+          logger.error('usePostByUri, error: ', error.value)
+          throw createError({ statusCode: 500, message: 'Error fetching PostByUri', fatal: true })
+      }
+      if (data?.value) {
+        logger.debug('usePostByUri, successfully fetched post: ', data.value.title)
+      } else {
+        logger.debug('usePostByUri, data is empty')
+      }
+      post.value = data.value
     }
-    logger.debug('usePostByUri: fetched post from WordPress: ', data.value.title)
     return {
-        data: data.value
+        data: post.value
     }
 }
 const _usePostById = async (id: number, asPreview?: boolean) => {
   const logger = useWPNuxtLogger()
+  const tokens = useTokens()
   const { data, pending, refresh, error } = await useFetch("/api/graphql_middleware/query/PostById/", {
     params: {
       id: id,
       asPreview: asPreview ? true : false
+    },
+    headers: {
+      Authorization: tokens.authorizationHeader
     },
     transform (data) {
       return data.data.post;
@@ -34,7 +59,11 @@ const _usePostById = async (id: number, asPreview?: boolean) => {
     logger.error('usePostByUId, error: ', error.value)
     throw createError({ statusCode: 500, message: 'Error fetching PostById', fatal: true })
   }
-  logger.debug('usePostById: fetched post from WordPress: ', data.value.title)
+  if (data?.value) {
+    logger.debug('usePostById, successfully fetched post: ', data.value.title)
+  } else {
+    logger.debug('usePostById, data is empty')
+  }
   return {
     data: data.value
   }
