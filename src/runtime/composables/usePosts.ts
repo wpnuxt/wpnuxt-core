5fa1f8de85eea448e9f3e72157a001f30f89f0a1
Wouter Vernaillen <wouter@vernaillen.com>
2024-01-15 14:13:55 +0700

    useLatestPost

--- src/runtime/composables/usePosts.ts
+++ src/runtime/composables/usePosts.ts
@@ -1,4 +1,4 @@
-import { useNuxtData, ref, useFetch, createError } from "#imports"
+import { useNuxtData, ref, useFetch, createError , useGraphqlQuery } from "#imports"
 
 const _usePosts = async () => {
     const cacheKey = 'allPosts'
@@ -23,5 +23,32 @@ const _usePosts = async () => {
         data: posts.value
     }
 }
+const _useLatestPost = async () => {
+    const cacheKey = 'latestPost'
+    const cachedPosts = useNuxtData(cacheKey)
+    const post = ref()
 
+    if (cachedPosts.data.value) {
+        post.value = cachedPosts.data.value
+        console.log('useLatestPost, got post from cache: ', post.value)
+    } else {
+        const { data, refresh, pending, error } = await useFetch("/api/graphql_middleware/query/LatestPost", {
+            key: cacheKey,
+            transform (data: any) {
+                return data.data.posts.nodes[0];
+            }
+        });
+        if (error.value) {
+            throw createError({ statusCode: 500, message: 'Error fetching latest post', fatal: true })
+        }
+        console.log('useLatestPost, successfully fetched post: ', data.value)
+        post.value = data.value
+    }
+    return {
+        data: post.value
+    }
+}
+
+
+export const useLatestPost = _useLatestPost
 export const usePosts = _usePosts
