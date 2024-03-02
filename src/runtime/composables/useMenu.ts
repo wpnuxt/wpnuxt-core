9e495961c768b567605c92ad229a0741d6ed8dc9
Wouter Vernaillen <wouter@vernaillen.com>
2024-03-02 19:23:48 +0100

    added useNuxtData cache in useMenu() composable

--- src/runtime/composables/useMenu.ts
+++ src/runtime/composables/useMenu.ts
@@ -1,30 +1,38 @@
-import { useFetch, createError, useRuntimeConfig } from "#imports"
+import { useFetch, createError, useRuntimeConfig, ref, useNuxtData, } from "#imports"
 import { useTokens } from "./useTokens";
 import { useWPNuxtLogger } from "./useWPNuxtlogger";
 
 const _useMenu = async (name?: string) => {
+    const menu = ref()
     const logger = useWPNuxtLogger()
     const config = useRuntimeConfig()
     const menuName = name && name.length > 0 ? name : config.public.wpNuxt.defaultMenuName
     const tokens = useTokens()
+    const cacheKey =  'menu-' + menuName
+    const cachedMenu = useNuxtData(cacheKey)
 
-    const { data, error } = await useFetch('/api/graphql_middleware/query/Menu', {
-        params: {
-          name: menuName
-        },
-        headers: {
-          Authorization: tokens.authorizationHeader
-        },
-        transform (data: any) {
-            return data.data.menu.menuItems.nodes;
-        }
-    });
-    if (error.value) {
-        logger.error('useMenu, error: ', error.value)
-        throw createError({ statusCode: error.value.status, message: error.value.message, fatal: true })
+    if (cachedMenu.data.value) {
+      menu.value = cachedMenu.data.value
+    } else {
+      const { data, error } = await useFetch('/api/graphql_middleware/query/Menu', {
+          params: {
+            name: menuName
+          },
+          headers: {
+            Authorization: tokens.authorizationHeader
+          },
+          transform (data: any) {
+              return data.data.menu.menuItems.nodes;
+          }
+      });
+      if (error.value) {
+          logger.error('useMenu, error: ', error.value)
+          throw createError({ statusCode: error.value.status, message: error.value.message, fatal: true })
+      }
+      menu.value = data.value
     }
     return {
-      data: data.value
+      data: menu.value
     }
 }
 
