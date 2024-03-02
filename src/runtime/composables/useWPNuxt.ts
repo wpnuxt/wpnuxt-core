79f6393678326c29597231ef58b9e5e59bd21a53
Wouter Vernaillen <wouter@vernaillen.com>
2024-03-02 16:19:21 +0100

    new STAGING env variable

--- src/runtime/composables/useWPNuxt.ts
+++ src/runtime/composables/useWPNuxt.ts
@@ -2,7 +2,8 @@ const _useWPNuxt = () => {
 
   return {
     isStaging: process.env.NODE_ENV === 'development'
-      || process.env.NODE_ENV === 'staging',
+      || process.env.NODE_ENV === 'staging'
+      || process.env.STAGING === 'true',
   }
 }
 
