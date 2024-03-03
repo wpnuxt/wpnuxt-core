9674f54f7938675888399842b860399c1ff4cb8f
Wouter Vernaillen <wouter@vernaillen.com>
2024-03-03 17:37:37 +0100

    27 example how to extend fragments (#34)
    * example how to extend fragments with Yoast SEO fields

---
+++ examples/yoastseo/test-src/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { usePosts } from '../../../../src/runtime/composables/usePosts'
+const posts = await usePosts()
+</script>
+
+<template>
+  <div>
+    <p>Extending Pages and Posts with meta data from Yoast SEO</p>
+    <div
+      v-for="post in posts.data"
+      :key="post.databaseId"
+    >
+      <h3>{{ post.title }}</h3>
+      <div>Yoast SEO title: {{ post.seo.title }}</div>
+      <div>Yoast SEO desc: {{ post.seo.metaDesc }}</div>
+    </div>
+  </div>
+</template>
