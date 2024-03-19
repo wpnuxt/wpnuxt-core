cf2894f7b195552f6c011eb2cc801e8fe1f3d381
Wouter Vernaillen <wouter@vernaillen.com>
2024-03-19 17:59:56 +0100

    better handling of featured images

---
+++ src/runtime/util/images.ts
@@ -0,0 +1,11 @@
+
+const getRelativeImagePath = function getRelativeImagePath(imgUrl: string): string {
+
+  if (imgUrl) {
+    const url = new URL(imgUrl);
+    imgUrl = url.pathname;
+  }
+  return imgUrl;
+}
+
+export { getRelativeImagePath }
