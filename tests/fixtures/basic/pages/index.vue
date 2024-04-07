eb4d30171c837e81d1dcdab34ed10e7d2eb83a0e
Wouter Vernaillen <wouter@vernaillen.com>
2024-04-07 09:15:25 +0200

    Set up test framework & add basic test

---
+++ tests/fixtures/basic/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useRuntimeConfig } from 'nuxt/app';
+
+const config = useRuntimeConfig();
+</script>
+
+<template>
+  <div>basic <span>{{ config.public.myValue }}</span></div>
+</template>
