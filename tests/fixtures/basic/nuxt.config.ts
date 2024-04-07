a62bf39b261545326aebb36a996af1bcabaf1578
Wouter Vernaillen <wouter@vernaillen.com>
2024-04-07 09:18:30 +0200

    Set up test framework & add basic test (#57)

---
+++ tests/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,12 @@
+import WPNuxtModule from '../../../src/module'
+
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      myValue: 'original value',
+    },
+  },
+  modules: [
+    WPNuxtModule
+  ]
+})
