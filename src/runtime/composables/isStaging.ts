f3807782c6148c8c1bf527ea871c6f97b96c7388
Wouter Vernaillen <wouter@vernaillen.com>
2024-04-16 18:09:47 +0200

    (temp) debug staging issue

--- src/runtime/composables/isStaging.ts
+++ src/runtime/composables/isStaging.ts
@@ -2,6 +2,7 @@ import { useRuntimeConfig } from '#imports'
 
 const _isStaging = async () => {
   const config = useRuntimeConfig()
+  console.log('isStaging.ts: ', config.public.wpNuxt)
   return config.public.wpNuxt.staging
 }
 export const isStaging = _isStaging
