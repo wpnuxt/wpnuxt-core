cdc7621f521ff08a9a715a256848ceb46f8c4756
Wouter Vernaillen <wouter@vernaillen.com>
2024-05-26 13:00:51 +0200

    remove console.log

--- src/runtime/composables/useFeaturedImage.ts
+++ src/runtime/composables/useFeaturedImage.ts
@@ -2,7 +2,6 @@ import { getRelativeImagePath } from '../util/images'
 import type { Post, Page } from '#graphql-operations'
 
 const _useFeaturedImage = (contentNode: Post | Page): string | undefined => {
-  console.log('from _useFeaturedImage', contentNode)
   const sourceUrl = contentNode?.featuredImage?.node?.sourceUrl
   if (sourceUrl) return getRelativeImagePath(sourceUrl)
   else return undefined
