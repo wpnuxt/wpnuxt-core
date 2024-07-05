f5f6defc2b3220f17ae05d9922efce586962c5da
Wouter Vernaillen <wouter@vernaillen.com>
2024-07-05 18:20:20 +0200

    change cache folder name to wpnuxt-content

--- src/runtime/server/storage.ts
+++ src/runtime/server/storage.ts
@@ -2,10 +2,10 @@ import { prefixStorage, type Storage } from 'unstorage'
 import { useLogger } from '../util/logger'
 import { useStorage } from '#imports'
 
-export const cacheStorage: Storage = prefixStorage(useStorage(), 'cache:content')
+export const cacheStorage: Storage = prefixStorage(useStorage(), 'cache:wpnuxt-content')
 
 export const purgeCache = async () => {
-  const keys = await cacheStorage.getKeys('cache:content')
+  const keys = await cacheStorage.getKeys('cache:wpnuxt-content')
   keys.forEach(async (key) => {
     await cacheStorage.removeItem(key)
   })
