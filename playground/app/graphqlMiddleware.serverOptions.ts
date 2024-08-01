408fb20a17c21ddcfede6e40638541872a42dadd
Wouter Vernaillen <wouter@vernaillen.com>
2024-08-01 09:18:07 +0200

    nuxt 4 folder structure for playground

---
+++ playground/app/graphqlMiddleware.serverOptions.ts
@@ -0,0 +1,15 @@
+import { defineGraphqlServerOptions } from 'nuxt-graphql-middleware/dist/runtime/serverOptions'
+import { getHeader } from 'h3'
+import type { H3Event } from 'h3'
+
+export default defineGraphqlServerOptions({
+
+  serverFetchOptions(event: H3Event) {
+    console.log('serverFetchOptions', event)
+    return {
+      headers: {
+        Authorization: getHeader(event, 'Authorization')
+      }
+    }
+  }
+})
